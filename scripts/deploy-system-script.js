46acece90b26c2653ec002483f644b2da8e065c5
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2020-12-20 20:09:59 +0100

    readme deploy (#8)

--- scripts/deploy-system-script.js
+++ scripts/deploy-system-script.js
@@ -26,14 +26,15 @@ async function main() {
 
     // transfers all tokens and ownership to new address
     const mainAddress = "0x3fD861afc57b2A3E0BCcbD39cFB4F3D88E798D42"
-    const tx1 = await contract.transferOwnership(mainAddress)
+    const tx1 = await contract.changeDollarCakeAddress(mainAddress)
+    console.log("fee address changed", tx1)
     await provider.waitForTransaction(tx1.hash)
-    console.log("ownership transfer", tx1)
-    const tx2 = await contract.transfer(mainAddress, "10000000000000000000000000")
+    const tx2 = await contract.transferOwnership(mainAddress)
     await provider.waitForTransaction(tx2.hash)
-    console.log("tokens tramsfer", tx2)
-    const tx3 = await contract.changeDollarCakeAddress(mainAddress)
-    console.log("fee address changed", tx3)
+    console.log("ownership transfer", tx2)
+    const tx3 = await contract.transfer(mainAddress, "10000000000000000000000000")
+    await provider.waitForTransaction(tx3.hash)
+    console.log("tokens tramsfer", tx3)
 
 }
 
