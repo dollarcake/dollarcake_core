6578eacefd36f3b52c72d0fb57739ace500e0165
JesseAbram <jesseabramowitz@hotmail.com>
2021-01-22 16:20:02 +0100

    more docs

--- contracts/staking/CakeStaking.sol
+++ contracts/staking/CakeStaking.sol
@@ -8,6 +8,9 @@ import "../openzeppelin/token/ERC20/SafeERC20.sol";
 import "../openzeppelin/math/SafeMath.sol";
 import "../openzeppelin/utils/ReentrancyGuard.sol";
 
+/// @title Cake Staking
+/// @author Jesse Abramowitz
+/// @notice the implementation of Dollar Cake Staking
 contract CakeStaking is ReentrancyGuard, CakeToken {
 
 	using SafeMath for uint256;
@@ -15,7 +18,7 @@ contract CakeStaking is ReentrancyGuard, CakeToken {
 
 	mapping (address => mapping (address => uint256)) public userStake; // content creator to user 
 	mapping (address => mapping (address => uint256)) public minActionTime; // user to content creator to time
-	mapping (address => uint256) public stakerSplit;
+	mapping (address => uint256) public stakerSplit; //  Porportion of payout stakers recieve
 	mapping (address => uint256) public creatorStaked; // amount of funds stakes for content creator
 	mapping (address => uint256) public contentTotalPayout; // amount of payouts for users of a CC
 
@@ -24,28 +27,27 @@ contract CakeStaking is ReentrancyGuard, CakeToken {
 	event UserDeposit(address indexed user, address indexed contentCreator, uint256 amountDespoited, uint256 payout);
 	event UserWithdrawal(address indexed user, address indexed contentCreator, uint256 payout, uint256 amountRecieved);
 
+	/**
+		Checks that arrays of content creators and amount is the same.
+		Checks that a rewarder is not delegating their votes. This prevents an infinite vote share mint exploit
+	 */
 	modifier rewardModifier(address[] memory _contentCreator, uint256[] memory _amount) {
 		require(_contentCreator.length == _amount.length, "mismatch");
 		require(delegatedFrom[msg.sender].amount == 0, "ERC20: reward from a delegated account");
 		_;
 	}
 
-    constructor(string memory _name, string memory _symbol) public CakeToken(_name, _symbol) {
-    }
+    constructor(string memory _name, string memory _symbol) public CakeToken(_name, _symbol) {}
 
-	function _msgSender(string memory _function, address address1, uint256 number1, address address2) internal override(GasStation) returns (address payable) {
-   		return super._msgSender(_function, address1, number1, address2);
-	}
-
-
+	/// @param _contentCreator an array of content creators to reward
+	/// @param _amount an array of amounts to reward
+	/// @notice rewards the content creators and bakers of the content creators
     function reward(address[] memory _contentCreator, uint256[] memory _amount) public nonReentrant rewardModifier(_contentCreator, _amount) {
-		for (uint256 i = 0; i < _contentCreator.length; i++) { 
-			uint256 _stakerSplit;
-			if (isControlingSplit) {
-				_stakerSplit = globalStakerSplit;
-			} else {
+		uint256 _stakerSplit = globalStakerSplit;
+		for (uint256 i = 0; i < _contentCreator.length; i++) {
+			if (!isControlingSplit) {
 				_stakerSplit = stakerSplit[_contentCreator[i]] == uint256(0) ? uint256(50) : stakerSplit[_contentCreator[i]];
-			}
+			} 
 			uint256 amountMinusFee = _amount[i].mul(fee).div(1000);
 			uint256 dollarCakeFee = _amount[i].sub(amountMinusFee);
 			uint256 stakerReward = amountMinusFee.mul(_stakerSplit).div(100);
@@ -59,6 +61,9 @@ contract CakeStaking is ReentrancyGuard, CakeToken {
 		}
     }
 
+	/// @param _contentCreator an array of content creators bakers to reward
+	/// @param _amount an array of amounts to reward
+	/// @notice rewards just the bakers of the content creators
 	function rewardStakingPoolOnly(address[] memory _contentCreator, uint256[] memory _amount) public nonReentrant rewardModifier(_contentCreator, _amount) {
 		for (uint256 i = 0; i < _contentCreator.length; i++) { 
 			creatorStaked[_contentCreator[i]] = creatorStaked[_contentCreator[i]].add(_amount[i]);
@@ -67,6 +72,8 @@ contract CakeStaking is ReentrancyGuard, CakeToken {
 		}
 	}
 
+	/// @param _newStakerPortion the porportion of your reward your bakers recieve 
+	/// @notice sets the porportion of your reward your bakers recieve
     function setSplit(uint256 _newStakerPortion) public nonReentrant {
 		require(_newStakerPortion <= 90 && _newStakerPortion >= 10, "not in bounds");
 		address payable sender = _msgSender("setSplit", address(0), _newStakerPortion, address(0));
@@ -76,6 +83,9 @@ contract CakeStaking is ReentrancyGuard, CakeToken {
 		emit SplitUpdated(sender, _newStakerPortion);
     }
 
+	/// @param _contentCreator content creator to baker to
+	/// @param _amount amount to bake
+	/// @notice bake to a content creator
     function deposit(address _contentCreator, uint256 _amount) public nonReentrant {
 		address payable sender = _msgSender("deposit", _contentCreator, _amount, address(0));
 		uint256 contractBalance = creatorStaked[_contentCreator];
@@ -84,6 +94,8 @@ contract CakeStaking is ReentrancyGuard, CakeToken {
 
 		uint256 payout;
 		if (contentTotalPayout[_contentCreator] == 0) {
+			// For auditor: I don't think this is needed, but Im worried about small numbers messing up the math, uint stuff
+			// that being said someone can with draw, all but 1 wei then skip this check, I wrote a test for that, so this should be unnecessary
 			require(_amount >= minInitialDeposit, "minimum first stake");
 			payout = _amount;
 		} else {
@@ -97,6 +109,9 @@ contract CakeStaking is ReentrancyGuard, CakeToken {
         // payoutIn / payoutTot = tokenAddedInd / total token (after paying)
     }
 
+	/// @param _contentCreator content creator to baker to	
+	/// @param _userStake amount to unstake
+	/// @notice unbake from a content creator
     function withdraw(address _contentCreator, uint256 _userStake) public nonReentrant {
 		address payable sender = _msgSender("withdraw", _contentCreator, _userStake, address(0));
 		require(now >= minActionTime[sender][_contentCreator], "wait more time");
@@ -109,10 +124,12 @@ contract CakeStaking is ReentrancyGuard, CakeToken {
 		emit UserWithdrawal(sender, _contentCreator, _userStake, payout);
     }
 
+	/// @dev helper function to turn a stake into a token balance
 	function withdrawPayout(address _contentCreator, uint256 _userStake) public view returns (uint256) {
 		return _userStake.mul(creatorStaked[_contentCreator]).div(contentTotalPayout[_contentCreator]);
 	}
 
+	/// @dev helper function to turn an array of stakes into a token balance
 	function withdrawable(address[] memory _contentCreators, address _user) public view returns (uint256[] memory) {
 		uint256[] memory withdrawableArray = new uint[](_contentCreators.length);
 		for (uint256 i = 0; i < _contentCreators.length; i++) { 
